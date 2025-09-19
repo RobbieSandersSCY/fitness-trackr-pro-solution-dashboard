b772019d2e35370f3539921a3906226146278c95
Lester Lee <contact@lester-lee.com>
2025-09-19 11:52:57 -0400

    Move delete button from ActivityListItem to ActivityDetails

--- src/activities/ActivityDetails.jsx
+++ src/activities/ActivityDetails.jsx
@@ -1,10 +1,14 @@
-import { useState, useEffect } from "react";
-import { getActivity } from "../api/activities";
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router";
+import { deleteActivity, getActivity } from "../api/activities";
+import { useAuth } from "../auth/AuthContext";
 
 export default function ActivityDetails() {
+  const { token } = useAuth();
   const { id } = useParams();
+  const navigate = useNavigate();
   const [activity, setActivity] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const syncActivity = async () => {
@@ -14,6 +18,17 @@ export default function ActivityDetails() {
     syncActivity();
   }, [id]);
 
+  const tryDelete = async () => {
+    setError(null);
+
+    try {
+      await deleteActivity(token, activity.id);
+      navigate("/activities");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
   if (!activity) return <p>Loading...</p>;
 
   return (
@@ -21,6 +36,8 @@ export default function ActivityDetails() {
       <h1>{activity.name}</h1>
       <p>by {activity.creatorName}</p>
       <p>{activity.description}</p>
+      {token && <button onClick={tryDelete}>Delete</button>}
+      {error && <p role="alert">{error}</p>}
     </article>
   );
 }
